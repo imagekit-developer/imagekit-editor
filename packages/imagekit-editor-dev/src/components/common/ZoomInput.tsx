2deab3d41ea38a94dd9f13c7292d7588cded9f76
Swarnim Doegar <swarnim@imagekit.io>
2026-02-11 20:46:50 +0530

    Move to strict type

--- packages/imagekit-editor-dev/src/components/common/ZoomInput.tsx
+++ packages/imagekit-editor-dev/src/components/common/ZoomInput.tsx
@@ -15,7 +15,7 @@ import { useEffect, useState } from "react"
 type ZoomInputFieldProps = {
   id?: string
   onChange: (value: number) => void
-  defaultValue?: number | unknown
+  defaultValue?: number
   value?: number
 }
 
